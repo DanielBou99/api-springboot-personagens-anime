128e4beb1b40923fa067831fd117d19c054d1361
Daniel <danielbou@outlook.com.br>
2021-11-27 14:52:01 -0300

    Projeto inicial

---
+++ personagens-anime/src/main/java/com/example/demo/PersonagensAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonagensAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonagensAnimeApplication.class, args);
+	}
+
+}
---
+++ personagens-anime/src/main/java/com/example/demo/controller/TesteController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TesteController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String Teste() {
+		return "Funcionou!";
+	}
+}
